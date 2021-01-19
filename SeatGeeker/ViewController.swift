bdd9916caf512bf8ceeaa004c9fb7ae308eeeb64
armangorjipoor <armangorjipoor@gmail.com>
2021-01-19 22:44:40 +0330

    Initial commit

---
+++ SeatGeeker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeatGeeker
+//
+//  Created by ArmanG on 10/30/1399 AP.
+//  Copyright © 1399 Pars Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
