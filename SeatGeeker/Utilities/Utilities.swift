7e5a20021dde4fb6a9ce3448457dfe12cbb194ad
armangorjipoor <armangorjipoor@gmail.com>
2021-01-20 19:48:35 +0330

    init commit

---
+++ SeatGeeker/Utilities/Utilities.swift
@@ -0,0 +1,44 @@
+//
+//  Utilities.swift
+//  SeatGeeker
+//
+//  Created by ArmanG on 11/1/1399 AP.
+//  Copyright © 1399 Pars Digital. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+func downloaded(from url: URL) {
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        guard
+            let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+            let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+            let data = data, error == nil,
+            let image = UIImage(data: data)
+            else { return }
+        DispatchQueue.main.async() { [weak self] in
+            self?.image = image
+        }
+    }.resume()
+}
+func downloaded(from link: String) {
+    guard let url = URL(string: link) else { return }
+    downloaded(from: url)
+}
+
+
+func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
