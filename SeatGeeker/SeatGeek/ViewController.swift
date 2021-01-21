e3aa56410e433f3dcae3bfe8580d1f6185169653
armangorjipoor <armangorjipoor@gmail.com>
2021-01-21 15:12:55 +0330

    some file changed

--- SeatGeeker/SeatGeek/ViewController.swift
+++ SeatGeeker/SeatGeek/ViewController.swift
@@ -8,26 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController {
     var event = [Event]()
     private var eventViewModel: ViewModel!
+    let searchController = UISearchController(searchResultsController: nil)
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchBar.tintColor = .white
+        searchController.searchBar.barTintColor = .green
+        eventViewModel = ViewModel()
+        getEvents()
         
     }
-    func getData() {
+    func getEvents() {
         
-        eventViewModel = ViewModel()
-        eventViewModel.getData{ welocom in
+        eventViewModel.getData { welocom in
             self.event = welocom.events
             DispatchQueue.main.async {
                 self.event = welocom.events
                 self.tableView.reloadData()
             }
         }
+        
     }
     
 }
@@ -38,13 +45,53 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.row
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SeatTableViewCell
-        cell.titleLbl.text = eventViewModel.createTitle(event: event[indexPath.row])
+        cell.titleLbl.text = eventViewModel.createTitle(index: event[indexPath.row])
         cell.cityLbl.text = event[indexPath.row].venue.city
         cell.dateLbl.text = event[indexPath.row].datetime_utc
         cell.seatImgView.downloaded(from: event[indexPath.row].performers.first!.image)
         return cell
     }
+}
+
+extension ViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("Log")
+        self.event.removeAll()
+        self.tableView.reloadData()
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("m;lm;lm")
+
+        eventViewModel.url = URL(string: "https://api.seatgeek.com/2/events?client_id=MjE1MDk2MTF8MTYxMTA4NTA5Ny40NDQ2NDM3&per_page=50")!
+        eventViewModel.getData { welocom in
+                   self.event = welocom.events
+                   DispatchQueue.main.async {
+                       self.event = welocom.events
+                       self.tableView.reloadData()
+                    print("Log neta \(welocom.meta.total)")
+                   }
+            print("Log: \(self.event.count)")
+               }
+        print("Log: \(event.count)")
+        tableView.reloadData()
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
     
     
 }
+
+
+/*
+ cell.titleLbl.text = eventViewModel.createTitle(index: indexPath.row)
+ cell.cityLbl.text = eventViewModel.eventsItem[index].venue.city
+ //event[indexPath.row].venue.city
+ cell.dateLbl.text = eventViewModel.eventsItem[index].datetime_utc
+ cell.seatImgView.downloaded(from: eventViewModel.eventsItem[index].performers.first!.image)
+ */
--- SeatGeeker/SeatGeek/ViewModel.swift
+++ SeatGeeker/SeatGeek/ViewModel.swift
@@ -10,37 +10,39 @@ import Foundation
 
 class ViewModel: NSObject {
     
+    var eventsItem = [Event]()
     private var apiService: APIServices!
-    private(set) var empData: Welcome! {
-        didSet {
-            self.bindViewModelToController()
-        }
-    }
-    private let url = URL(string: "https://api.seatgeek.com/2/events?client_id=MjE1MDk2MTF8MTYxMTA4NTA5Ny40NDQ2NDM3&per_page=10")!
-    var eventItem = [Event]()
+//    private(set) var empData: Welcome! {
+//        didSet {
+//            self.bindViewModelToController()
+//        }
+//    }
+     var url = URL(string: "https://api.seatgeek.com/2/events?client_id=MjE1MDk2MTF8MTYxMTA4NTA5Ny40NDQ2NDM3&per_page=10")!
     
-    var bindViewModelToController: (() -> ()) = { }
+   // var bindViewModelToController: (() -> ()) = { }
     
     override init() {
         super.init()
-        self.apiService = APIServices()
+       // self.apiService = APIServices()
        
     }
     
     
     func getData(completion: @escaping (Welcome) -> ()) {
+        print(eventsItem.count)
         URLSession.shared.dataTask(with: url ) {
             data, respone , error in
             if let data = data {
                 let jsonDecoder = JSONDecoder()
                 let eventData = try! jsonDecoder.decode(Welcome.self, from: data)
+                self.eventsItem = eventData.events
                 completion(eventData)
             }
         }.resume()
     }
     
-    func createTitle(event: Event) -> String{
-        event.type + " at " + event.venue.city
+    func createTitle(index: Event) -> String{
+        return index.type + " at " + index.venue.city
     }
     
 }
